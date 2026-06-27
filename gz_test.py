20ed2a2598b15680fb4ccc5123a63e4a4a973514
Elizabeth Terveen <eterveen@andrew.cmu.edu>
2026-06-27 20:10:23 +0000

    unit tests for bot sim capabilties

---
+++ gz_test.py
@@ -0,0 +1,30 @@
+# custom message type unit test 
+def custom_handkshake():
+    pass 
+
+# respecting physics unit test 
+def publish_no_go():
+    """ publish bounds of viable bot location (3D filter)  """ 
+    pass 
+
+def assert_bot_in_bounds():
+    """ bot position should not overlap with no go zone """
+    pass 
+
+def assert_not_volatile():
+    """ bot position should not change dramatically """ 
+    pass
+
+# sensor data unit tests
+def assert_lidar_data():
+    """ receive data on the lidar topic """ 
+    pass
+
+def assert_camera_data():
+    """ receive camera data """ 
+    pass
+
+def assert_pose_data():
+    """ receive robot pose """ 
+    pass
+
